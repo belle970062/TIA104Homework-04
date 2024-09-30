a3d65fd57c5cd46073998173985a5318f3edda71
Belle Lee <belle970062@gmail.com>
2024-09-30 16:06:41 +0800

    homework0930

---
+++ src/homework1/hw1_1.java
@@ -0,0 +1,9 @@
+package homework1;
+
+public class hw1_1 {
+	public static void main(String[] args) {
+		int num1 = 12, num2 = 6;
+		System.out.println("12+6=" + (num1 + num2));
+		System.out.println("12x6=" + (num1 * num2));
+	}
+}
---
+++ src/homework1/hw1_2.java
@@ -0,0 +1,9 @@
+package homework1;
+
+public class hw1_2 {
+	public static void main(String[] args) {
+		int num1 = 200, num2 = 12;
+		System.out.println("200顆蛋為" + (num1 / num2) + "打" + (num1 % num2) + "顆");
+	}
+
+}
